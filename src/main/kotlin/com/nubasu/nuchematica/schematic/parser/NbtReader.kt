1497ba0eeb71b0cdfbe03785e42106040248981c
nubasu <Augelevth@gmail.com>
2024-04-14 18:12:37 +0900

    refactor NbtReader

--- src/main/kotlin/com/nubasu/nuchematica/schematic/parser/NbtReader.kt
+++ src/main/kotlin/com/nubasu/nuchematica/schematic/parser/NbtReader.kt
@@ -1,48 +1,42 @@
 package com.nubasu.nuchematica.schematic.parser
 
 import com.nubasu.nuchematica.tag.*
+import java.io.Closeable
 import java.io.DataInputStream
 import java.io.IOException
 
-public class NbtReader(private val input: DataInputStream) {
+public class NbtReader(private val input: DataInputStream): Closeable {
     public fun readByte(): ByteTag {
         val b = input.readByte()
-        println("readByte: $b")
         return ByteTag(b)
     }
 
     public fun readShort(): ShortTag {
         val s = input.readShort()
-        println("readShort: $s")
         return ShortTag(s)
     }
 
     public fun readInt(): IntTag {
         val i = input.readInt()
-        println("readInt: $i")
         return IntTag(i)
     }
 
     public fun readLong(): LongTag {
         val l = input.readLong()
-        println("readLong: $l")
         return LongTag(l)
     }
 
     public fun readFloat(): FloatTag {
         val f = input.readFloat()
-        println("readFloat: $f")
         return FloatTag(f)
     }
 
     public fun readDouble(): DoubleTag {
         val d = input.readDouble()
-        println("readDouble: $d")
         return DoubleTag(d)
     }
 
     public fun readByteArray(): ByteArrayTag {
-        println("readByteArray")
         val length = input.readInt()
         val bytes = ByteArray(length)
         input.readFully(bytes)
@@ -54,30 +48,24 @@ public class NbtReader(private val input: DataInputStream) {
         val bytes = ByteArray(length.toInt())
         input.readFully(bytes)
         val s = String(bytes, TagDefinition.CHARSET)
-        println("readString: $s")
         return StringTag(s)
     }
 
     public fun readList(): ListTag {
-        println("readList")
         val tagId = input.readByte().toInt()
         val length = input.readInt()
         val tagList = arrayListOf<Tag>()
         for (i in 0 until length) {
-            parseTag(tagId)
+            tagList.add(parseTag(tagId))
         }
         return ListTag(TagDefinition.getTagClass(tagId), tagList)
     }
 
     public fun readCompoundTag(): CompoundTag {
-        println("readCompoundTag")
         var tagId = input.readByte().toInt()
         val content: MutableMap<String, Tag> = mutableMapOf()
         while (tagId != 0) {
-            println("tag id: $tagId")
             val tagName = readTagName()
-            println("tag name: $tagName")
-
             val tag = parseTag(tagId)
             content[tagName] = tag
 
@@ -91,7 +79,6 @@ public class NbtReader(private val input: DataInputStream) {
     }
 
     public fun readIntArray(): IntArrayTag {
-        println("readIntArray")
         val length = input.readInt()
         val intArray = arrayListOf<Int>()
         for (i in 0 until length) {
@@ -101,7 +88,6 @@ public class NbtReader(private val input: DataInputStream) {
     }
 
     public fun readLongArray(): LongArrayTag {
-        println("readLongArray")
         val length = input.readInt()
         val longArray = arrayListOf<Long>()
         for (i in 0 until length) {
@@ -119,10 +105,7 @@ public class NbtReader(private val input: DataInputStream) {
 
     private fun parseTag(tagId: Int): Tag {
         return when (tagId) {
-            TagDefinition.TAG_END.id -> {
-                println("tag end")
-                EndTag()
-            }
+            TagDefinition.TAG_END.id -> EndTag()
             TagDefinition.TAG_BYTE.id -> readByte()
             TagDefinition.TAG_SHORT.id -> readShort()
             TagDefinition.TAG_INT.id -> readInt()
@@ -137,5 +120,9 @@ public class NbtReader(private val input: DataInputStream) {
             TagDefinition.TAG_LONG_ARRAY.id -> readLongArray()
             else -> throw IOException("Invalid tag type: $tagId.")
         }
+    }
+
+    override fun close() {
+        input.close()
     }
 }
