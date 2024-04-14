32cf4e060660b5a4bb7fda661d4ba4c5394dcb9c
nubasu <Augelevth@gmail.com>
2024-04-14 14:34:22 +0900

    add NbtReaderTest

---
+++ src/test/kotlin/NbtReaderTest.kt
@@ -0,0 +1,17 @@
+import com.nubasu.nuchematica.schematic.parser.NbtReader
+import net.minecraft.util.FastBufferedInputStream
+import org.junit.jupiter.api.Test
+import java.io.DataInputStream
+import java.io.FileInputStream
+
+public class NbtReaderTest {
+
+    @Test
+    public fun testReadCompoundTag() {
+        val path = javaClass.getResource("test_schematic/0_a.schematic").path
+        val inputStream = DataInputStream(FastBufferedInputStream(FileInputStream(path)))
+
+        val reader = NbtReader(inputStream)
+        reader.readCompoundTag()
+    }
+}
