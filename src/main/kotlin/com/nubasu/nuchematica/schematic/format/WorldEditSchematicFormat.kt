14bbe21ebefc277a49ff0f05b57ef28a8fa329d7
nubasu <Augelevth@gmail.com>
2024-04-14 19:34:47 +0900

    change to not use tags

--- src/main/kotlin/com/nubasu/nuchematica/schematic/format/WorldEditSchematicFormat.kt
+++ src/main/kotlin/com/nubasu/nuchematica/schematic/format/WorldEditSchematicFormat.kt
@@ -4,20 +4,20 @@ import com.nubasu.nuchematica.schematic.SchematicFormat
 import com.nubasu.nuchematica.tag.*
 
 public data class WorldEditSchematicFormat(
-    val name: StringTag,
-    val materials: StringTag,
-    val width: ShortTag,
-    val height: ShortTag,
-    val length: ShortTag,
-    val weOriginX: IntTag?,
-    val weOriginY: IntTag?,
-    val weOriginZ: IntTag?,
-    val weOffsetX: IntTag?,
-    val weOffsetY: IntTag?,
-    val weOffsetZ: IntTag?,
-    val blocks: ByteArrayTag,
-    val data: ByteArrayTag,
-    val addBlocks: ByteArrayTag?,
-    val tileEntities: CompoundTag?,
-    val entities: CompoundTag?
+    val name: String,
+    val materials: String,
+    val width: Short,
+    val height: Short,
+    val length: Short,
+    val weOriginX: Int?,        // unsupported in this mod
+    val weOriginY: Int?,        // unsupported in this mod
+    val weOriginZ: Int?,        // unsupported in this mod
+    val weOffsetX: Int?,        // unsupported in this mod
+    val weOffsetY: Int?,        // unsupported in this mod
+    val weOffsetZ: Int?,        // unsupported in this mod
+    val blockIds: ByteArray,
+    val blockData: ByteArray,
+    val addBlocks: ByteArray?,  // unsupported in this mod
+    val tileEntities: List<Tag>,
+    val entities: List<Tag>
 ): SchematicFormat
