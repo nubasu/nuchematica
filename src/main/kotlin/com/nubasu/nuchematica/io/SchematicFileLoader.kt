ec61bed5d47e3134942196dd48f9619124a4c734
nubasu <Augelevth@gmail.com>
2025-05-23 21:08:10 +0900

    feat: add SchematicFileLoader for loading schematic file

---
+++ src/main/kotlin/com/nubasu/nuchematica/io/SchematicFileLoader.kt
@@ -0,0 +1,47 @@
+package com.nubasu.nuchematica.io
+
+import com.nubasu.nuchematica.common.SchematicCache
+import com.nubasu.nuchematica.schematic.SchematicHolder
+import com.nubasu.nuchematica.schematic.reader.WorldEditSchematicReader
+import it.unimi.dsi.fastutil.io.FastBufferedInputStream
+import net.minecraft.client.Minecraft
+import net.minecraft.core.BlockPos
+import net.minecraft.world.level.block.entity.BlockEntity
+import net.minecraft.world.level.block.state.BlockState
+import java.io.DataInputStream
+import java.io.FileInputStream
+
+public object SchematicFileLoader {
+    private val shemDir = Minecraft.getInstance().gameDirectory.absolutePath + "/schematics"
+
+    public fun loadRenderBlocks(schematicFile: String) {
+        val inputStream = DataInputStream(FastBufferedInputStream(FileInputStream("$shemDir/$schematicFile")))
+        val compoundTag = NbtReader(inputStream).readCompoundTag()
+        val clipboard = WorldEditSchematicReader.read(compoundTag)
+        inputStream.close()
+
+        val blocks = mutableMapOf<BlockPos, BlockState>()
+        val blockEntities = mutableMapOf<BlockPos, BlockEntity?>()
+
+        for (i in 0 until clipboard.position.size) {
+            val block = clipboard.block[i]
+            val blockEntity = clipboard.tileEntity[i]
+
+            if (!block.isAir) {
+                val pos = clipboard.position[i]
+                val relativeBlockPos = BlockPos(
+                    pos.x,
+                    pos.y,
+                    pos.z
+                )
+                if (blockEntity != null) {
+                    blockEntities[relativeBlockPos] = clipboard.tileEntity[i]
+                } else {
+                    blocks[relativeBlockPos] = clipboard.block[i]
+                }
+            }
+        }
+
+        SchematicHolder.schematicCache = SchematicCache(blocks, blockEntities)
+    }
+}
